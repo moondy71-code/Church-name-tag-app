9c41e442da72c49f05782c58ded23420b73097e2
SeongJoon Moon <moondy71@gmail.com>
2026-04-18 14:54:44 +0200

    update favicon

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { registerSW } from "virtual:pwa-register"; // ⭐ 추가
 
 createRoot(document.getElementById("root")!).render(<App />);
+
+// ⭐ 이 줄 추가 (맨 아래)
+registerSW({ immediate: true });
